fb82278a8062375a2158ad6a5e104478904d0a85
Dizaled <silifslikirill@gmail.com>
2025-02-21 16:12:01 +0700

    Не добовляется несколько заказов

---
+++ src/main/java/org/example/mrdverkin/controllers/ListOrderController.java
@@ -0,0 +1,25 @@
+package org.example.mrdverkin.controllers;
+
+import org.example.mrdverkin.dataBase.Order;
+import org.example.mrdverkin.dataBase.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/listOrders")
+public class ListOrderController {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping
+    public String listOrders(Model model) {
+        List<Order> ordes = orderRepository.findAll();
+        model.addAttribute("orders", ordes);
+        return "listOrders";
+    }
+}
