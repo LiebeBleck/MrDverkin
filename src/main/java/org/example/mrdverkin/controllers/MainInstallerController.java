4d3cb2e279a90d6745e4acc046a510ffaabdc9af
Dizaled <Silifslikirill@gmail.com>
2025-03-01 13:21:32 +0700

    update-calendare

--- src/main/java/org/example/mrdverkin/controllers/MainInstallerController.java
+++ src/main/java/org/example/mrdverkin/controllers/MainInstallerController.java
@@ -41,7 +41,7 @@ public class MainInstallerController {
         List<Order> ordes = orderRepository.findByInstallerNull();
         LocalDate today = LocalDate.now();
         List<DateAvailability> availabilityList = new ArrayList<>();
-        for (int i = 0; i < 7; i++ ){
+        for (int i = 0; i < 14; i++ ){
             DateAvailability availability = new DateAvailability(today, orderRepository.numberOfDoorsToInstallation(today));
             availabilityList.add(availability);
             today = today.plusDays(1);
