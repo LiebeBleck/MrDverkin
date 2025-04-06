acd0bafa1ff96f33835ba7f11994b4747e2e3a09
Dizaled <silifslikirill@gmail.com>
2025-04-06 13:24:15 +0700

    test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,6 @@ INSERT INTO installer (full_name, phone) VALUES ('Иван Петров', '79231
 INSERT INTO installer (full_name, phone) VALUES ('Павел Деревянка', '79994673033');
 INSERT INTO installer (full_name, phone) VALUES ('Павел Воля', '79137439007');
 
-
 insert into "order" (address, phone, DATE_ORDER ,  inDoorQuantity,frontDoorQuantity) values ('Ул. Пушкина, д. Колатушкина', '88005553535', '2025-03-06', '70','70');
 insert into "order" (address, phone, DATE_ORDER  , inDoorQuantity,frontDoorQuantity) values ('Ул. Пушкина, д. Колатушкина', '88005553535', '2025-03-11', '70','70');
 insert into "order" (address, phone, DATE_ORDER  , inDoorQuantity,frontDoorQuantity) values ('Ул. Пушкина, д. Колатушкина', '88005553535', '2025-03-01', '70','70');
