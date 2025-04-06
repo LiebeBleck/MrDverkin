77da255db9d0603adfd59c689414d6f7c719c684
Dizaled <silifslikirill@gmail.com>
2025-04-06 13:02:40 +0700

    тест

--- src/test/java/org/example/mrdverkin/MrDverkinApplicationTests.java
+++ src/test/java/org/example/mrdverkin/MrDverkinApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.mrdverkin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MrDverkinApplication.class)
 class MrDverkinApplicationTests {
 
     @Test
