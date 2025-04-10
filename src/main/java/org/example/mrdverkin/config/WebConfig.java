de97017d9d9e5b51eb0810a0b44c740fba892244
Dizaled <Silifslikirill@gmail.com>
2025-04-11 00:54:00 +0700

    Добавлен API controller

--- src/main/java/org/example/mrdverkin/config/WebConfig.java
+++ src/main/java/org/example/mrdverkin/config/WebConfig.java
@@ -1,6 +1,8 @@
 package org.example.mrdverkin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
--- src/main/java/org/example/mrdverkin/dataBase/Repository/InstallerRepository.java
+++ src/main/java/org/example/mrdverkin/dataBase/Repository/InstallerRepository.java
@@ -1,7 +1,6 @@
 package org.example.mrdverkin.dataBase.Repository;
 
 import org.example.mrdverkin.dataBase.Entitys.Installer;
-import org.example.mrdverkin.dataBase.Entitys.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
