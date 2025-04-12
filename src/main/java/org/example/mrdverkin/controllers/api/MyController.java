615979b1fd2cbccbabab08876fbf78ae4441c101
Dizaled <silifslikirill@gmail.com>
2025-04-12 20:24:57 +0700

    api controller update

--- src/main/java/org/example/mrdverkin/controllers/api/MyController.java
+++ src/main/java/org/example/mrdverkin/controllers/api/MyController.java
@@ -1,7 +1,9 @@
 package org.example.mrdverkin.controllers.api;
 
+import org.example.mrdverkin.dto.LoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Привет от Spring!");
+    @GetMapping("/login")
+    public ResponseEntity<String> sayHello(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+        return ResponseEntity.ok("Login:" + username + ", password:" + password);
     }
 }
 
